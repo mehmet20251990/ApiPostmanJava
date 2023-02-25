9680a43570f7c69cbe9231d277b2e20aff715a5a
mehmet20251990 <mehmet20251990@gmail.com>
2023-02-25 21:30:12 +0300

    update

---
+++ src/test/java/baseURLs/TheMovieDBBaseURL.java
@@ -0,0 +1,12 @@
+package baseURLs;
+
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.specification.RequestSpecification;
+import org.junit.Before;
+public class TheMovieDBBaseURL {
+    protected RequestSpecification specification;
+    @Before
+    public void setUpBaseURL(){
+        specification = new RequestSpecBuilder().setBaseUri("https://api.themoviedb.org/3").build();
+    }
+}

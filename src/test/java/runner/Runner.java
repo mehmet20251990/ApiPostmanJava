b585406661c52421f061c49f225e0820fda03903
mehmet20251990 <mehmet20251990@gmail.com>
2023-03-07 23:39:05 +0300

    update

---
+++ src/test/java/runner/Runner.java
@@ -0,0 +1,20 @@
+package runner;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        strict = true,
+        plugin = {
+                "pretty", //Steplerimi konsolda görebilmek için
+        },
+        //path of feature
+        features = "src/test/resources",
+        //path of step definitons
+        glue = "trelloAPI",
+        tags = "@createboard",
+        dryRun = true
+                )
+public class Runner {
+}
